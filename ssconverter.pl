52716c221a05858810a52f0711bafb3a94cacad5
Jerry Champlin <jchamplin@absolute-performance.com>
2011-05-26 23:01:42 +0000

    Initial commit

---
+++ ssconverter.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+if ( $#ARGV < 2 || $ARGV[0] =~ /^\-h$/ ) {
+  print "Usage:\n";
+  print "ssconverter.pl <inputfile.csv> <outputfile.xls>\n";
+  print "Generates EXCEL 2003-2007 format xls binary files\n";
+}
+
+else {}
